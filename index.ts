a65538f806973f76fb72f0d6cf0f90ef2da542d5
Farman Shaik <shkfrmn786@gmail.com>
2025-04-19 05:41:54 +0530

    fix:path

--- index.ts
+++ index.ts
@@ -22,22 +22,22 @@ const port = 5000;
 app.use(express.json())
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use('/uploads', express.static(path.join(__dirname, "../uploads")));
 
 app.use('/api', auth)
 
 app.get('/admin', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get('/verification', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get('/login', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, () => {
